e74974fde9bd1522c65e812c77b4c255014bace9
zidvsd <rashidvisda@gmail.com>
2025-03-09 19:55:12 +0800

    Added navigation bar

---
+++ js/main.js
@@ -0,0 +1,5 @@
+import { searchShortcut } from "./shortcuts.js";
+
+window.onload = () => {
+  searchShortcut();
+};
---
+++ js/shortcuts.js
@@ -0,0 +1,26 @@
+export const searchShortcut = () => {
+  const searchInputs = document.querySelectorAll(".search-input");
+
+  const displaySearch = () => {
+    searchInputs.forEach((input) => {
+      input.focus();
+    });
+  };
+
+  const hideSearch = () => {
+    searchInputs.forEach((input) => {
+      input.blur();
+    });
+  };
+
+  document.addEventListener("keydown", (event) => {
+    if ((event.ctrlKey || event.metaKey) && event.key === "k") {
+      event.preventDefault();
+      displaySearch();
+    }
+
+    if (event.key === "Escape") {
+      hideSearch();
+    }
+  });
+};
